0993fd5d39ca2a7b24fcf67042539adf4f297825
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2023-10-16 06:05:05 -0300

    feat: tests

---
+++ src/test/java/com/pickpaysimplify/PickpaysimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickpaysimplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpaysimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
