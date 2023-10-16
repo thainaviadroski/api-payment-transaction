8b17671a2ad4e27909c0fc48feec4eb5176c85a2
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2023-10-16 05:59:16 -0300

    First Commit

---
+++ src/main/java/com/pickpaysimplify/PickpaysimplifyApplication.java
@@ -0,0 +1,13 @@
+package com.pickpaysimplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpaysimplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpaysimplifyApplication.class, args);
+	}
+
+}
